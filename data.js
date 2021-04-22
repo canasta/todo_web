0d8827538afff4aaab08345b3783af5ed89de79d
root <root@nyang.party>
2021-04-22 17:05:22 +0900

    Split init() to update() and Add update timer

--- data.js
+++ data.js
@@ -1,56 +1,64 @@
 var gItemList = {
 	"0": {
-		"text": "[5]출석",
+		"text": "[5] 출석",
 		"img": "",
+		"color": "#3fab7a",
 		"term": "daily",
 		"renewal": 0,
 		"until": "2021-05-31"
 	},
 	"1": {
-		"text": "[15]무작위 임무: 레벨링 또는 무작위 임무: 숙련자",
+		"text": "[15] 무작위 임무: 레벨링 또는 무작위 임무: 숙련자",
 		"img": "",
+		"color": "#e0874f",
 		"term": "daily",
 		"renewal": 0,
 		"until": "2021-05-31"
 	},
 	"2": {
-		"text": "[10]신생 지역 돌발 5회",
+		"text": "[10] 신생 지역 돌발 5회",
 		"img": "",
+		"color": "#4c89d4",
 		"term": "daily",
 		"renewal": 0,
 		"until": "2021-05-31"
 	},
 	"3": {
-		"text": "[10]창천 지역 돌발 5회",
+		"text": "[10] 창천 지역 돌발 5회",
 		"img": "",
+		"color": "#4c89d4",
 		"term": "daily",
 		"renewal": 0,
 		"until": "2021-05-31"
 	},
 	"4": {
-		"text": "[10]홍련 지역 돌발 5회",
+		"text": "[10] 홍련 지역 돌발 5회",
 		"img": "",
+		"color": "#4c89d4",
 		"term": "daily",
 		"renewal": 0,
 		"until": "2021-05-31"
 	},
 	"5": {
-		"text": "[30]크리스탈 타워:고대인의 미궁",
+		"text": "[30] 크리스탈 타워: 고대인의 미궁",
 		"img": "",
+		"color": "#ad6ae6",
 		"term": "daily",
 		"renewal": 0,
 		"until": "2021-05-31"
 	},
 	"6": {
-		"text": "[30]크리스탈 타워:시르쿠스 탑",
+		"text": "[30] 크리스탈 타워: 시르쿠스 탑",
 		"img": "",
+		"color": "#ad6ae6",
 		"term": "daily",
 		"renewal": 0,
 		"until": "2021-05-31"
 	},
 	"7": {
-		"text": "[30]크리스탈 타워: 어둠의 세계",
+		"text": "[30] 크리스탈 타워: 어둠의 세계",
 		"img": "",
+		"color": "#ad6ae6",
 		"term": "daily",
 		"renewal": 0,
 		"until": "2021-05-31"
--- todo.js
+++ todo.js
@@ -1,5 +1,6 @@
 var gCheckDatetime = {};
 
+
 function init() {
 	try {
 		const checkDatetime = JSON.parse(localStorage.checkDatetime);
@@ -9,16 +10,65 @@ function init() {
 		gCheckDatetime = {};
 	}
 
-	var listdiv = document.getElementById("listDiv");
+	let listdiv = document.getElementById("listDiv");
 	const template = document.querySelector("#template-todo-item").innerHTML;
+	
+	let maxTextId;
+	let maxTextLen = 0;
 	for (const id in gItemList) {
+		if (maxTextLen < gItemList[id]["text"].length) {
+			maxTextId = id;
+			maxTextLen = gItemList[id]["text"].length;
+		}
+
 		let itemhtml = template.replaceAll("{{todo-item-id}}", id)
 			.replaceAll("{{todo-item-text}}", gItemList[id]["text"]);
 
+		// 아이템 색상 설정
+		if ("color" in gItemList[id])
+			itemhtml = itemhtml.replaceAll("{{todo-item-color}}", "background-color: " + gItemList[id]["color"] + " !important;");
+		else
+			itemhtml = itemhtml.replaceAll("{{todo-item-color}}", "");
+
+/*
+		// 이미지 주소 체크
+		if ("img-src" in gItemList[id])
+			itemhtml = itemhtml.replaceAll("{{todo-item-img-src}}", gItemList[id]["img"]);
+		else
+			itemhtml = itemhtml.replaceAll("{{todo-item-img-src}}", "./default-item.jpg");
+*/
+
+		listdiv.innerHTML += itemhtml;
+	}
+
+	let new_style = document.createElement('style');
+	document.head.appendChild(new_style);
+
+	let maxLenDiv = document.getElementById("itemDiv_" + maxTextId);
+	let fontPercent = 100 * Math.max(
+		maxLenDiv.clientWidth / maxLenDiv.firstElementChild.firstElementChild.offsetWidth,
+		maxLenDiv.clientHeight / maxLenDiv.firstElementChild.firstElementChild.offsetHeight / 2
+	)
+	new_style.sheet.insertRule("span {font-size: " + fontPercent + "%;}");
+
+	// 세로폭 조정
+	if (document.body.clientHeight > document.getElementById("listDiv").clientHeight) {
+		let divHeight = 100 / Math.ceil(Object.keys(gItemList).length / 2)
+		new_style.sheet.insertRule("div#listDiv {height: 100% !important;}");
+		new_style.sheet.insertRule("div.itemDiv {height: " + divHeight + "% !important;}");
+	}
+
+	update();
+}
+
+
+function update() {
+	let current = new Date();
+	current.setMinutes(0, 0, 0);
+	let renewal;
+
+	for (const id in gItemList) {
 		// 마지막 기록한 시간 체크
-		let current = new Date();
-		current.setMinutes(0, 0, 0);
-		let renewal;
 
 		if (gItemList[id]["term"] == "daily") {
 			renewal = current.setHours(gItemList[id]["renewal"]);
@@ -36,25 +86,20 @@ function init() {
 		}
 		renewal = new Date(renewal);
 
-		if (id in gCheckDatetime 
+		if (id in gCheckDatetime
 			&& new Date(gCheckDatetime[id]) > renewal)
-			itemhtml = itemhtml.replaceAll("{{todo-item-visibility}}", "hidden");
+			document.getElementById("itemDiv_" + id).classList.add("hidden");
 		else
-			itemhtml = itemhtml.replaceAll("{{todo-item-visibility}}", "");
-
-		// 이미지 주소 체크
-		if ("img-src" in gItemList[id])
-			itemhtml = itemhtml.replaceAll("{{todo-item-img-src}}", gItemList[id]["img"]);
-		else
-			itemhtml = itemhtml.replaceAll("{{todo-item-img-src}}", "./default-item.jpg");
-
-		listdiv.innerHTML += itemhtml;
+			document.getElementById("itemDiv_" + id).classList.remove("hidden");
 	}
+
+	let refreshTime = new Date().setHours(24,0,0,0) - new Date();
+	window.setTimeout(update, refreshTime);
 }
 
 
 function toggleItem(id) {
-	const res = document.getElementById(id).classList.toggle("hidden");
+	const res = document.getElementById("itemDiv_" + id).classList.toggle("hidden");
 
 	if (res)
 		gCheckDatetime[id] = new Date();
